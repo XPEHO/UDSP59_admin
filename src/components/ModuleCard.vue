8f96466591925c56d722c399de82263e4a8621da
Ryan Debouvries <debouvriesryan@gmail.com>
2023-12-22 17:01:51 +0100

    feat(front_module_page): Make the front for modules and module and set the routing between the concerned view

---
+++ src/components/ModuleCard.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import { Module } from '../models/Module';
+
+
+defineProps({
+  id: {
+    type: String,
+    required: true,
+  },
+  module: {
+    type: Module,
+    required: true,
+  },
+});
+
+</script>
+
+<template>
+  <div class="module-card">
+    <p>{{ module.title }}</p>
+    <a href="javascript:void(0)" @click=""><img src="../assets/delete.svg"></a>
+    <RouterLink :to="'/module/' + id"><img src="../assets/chevron-right.svg"></RouterLink>
+  </div>
+</template>
+
+<style>
+.module-card {
+  background-color: var(--color-primary);
+  color: var(--color-background);
+  padding: 1.2rem 2rem;
+  border-radius: 1rem;
+  width: 100%;
+  box-shadow: 3px 3px 10px 1px rgb(0 0 0 / 40%);
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+  align-items: center;
+  gap: 1.5rem;
+
+  & p {
+    font-size: 15pt;
+    width: 100%;
+  }
+
+  & a {
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    & img {
+      height: 1.8rem;
+      width: 1.8rem;
+    }
+  }
+}
+</style>
--- src/views/ModuleView.vue
+++ src/views/ModuleView.vue
@@ -2,27 +2,30 @@
 import { useDataStore } from '@/stores/data';
 import { onMounted, ref } from 'vue';
 import Loader from '@/components/Loader.vue';
-import ModuleCard from '@/components/ModuleCard.vue';
+import { useRoute } from 'vue-router';
+import ModulePartCard from '@/components/ModulePartCard.vue';
 
 const dataStore = useDataStore()
+const route = useRoute();
 
 const isLoading = ref(true);
 onMounted(async () => {
-  await dataStore.loadModulesFromFirebase();
+  await dataStore.loadModuleFromFirebase(route.params.id as string);
   isLoading.value = false;
 });
 
 </script>
 
 <template>
-  <main class="modules-view">
+  <main class="module-view">
     <Loader v-if="isLoading" />
-    <ModuleCard v-else v-for="[id, module] in dataStore.modules" :id="id" :module="module" />
+    <ModulePartCard v-else v-for="(modulePart, index) in dataStore.module.parts" :id="(route.params.id as string)"
+      :modulePart="modulePart" :index="index" />
   </main>
 </template>
 
 <style>
-main.modules-view {
+main.module-view {
   padding: 1rem 1rem 2rem 1rem;
   display: flex;
   flex-direction: column;
