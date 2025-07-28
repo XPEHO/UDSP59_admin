3037f3aa0a2666d8ce8d32e64d3b242686582a35
Théo Lebègue <theo.lebegue@icloud.com>
2025-07-28 11:48:17 +0200

    feat(alertNotAdmin): add a pop up alert if you're not admin

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
+import UnauthorizedAlertPopup from "@/components/UnauthorizedAlertPopup.vue";
 </script>
 
 <template>
   <RouterView />
+  <UnauthorizedAlertPopup />
 </template>
 
 <style scoped></style>
